9c886335f3056268f1a83a05bde1666fb8299d09
Joel-Tola <SLjoel.ts@gmail.com>
2025-04-20 21:56:39 +0100

    Refactor navigation component and styles for improved responsiveness and organization

--- nextjs-webapp/src/app/layout.tsx
+++ nextjs-webapp/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Nav } from "@/components/Nav";
-import { geistSans, geistMono } from "@/utils/fonts";
+import { mulish } from "@/utils/fonts";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.className} ${geistMono.className} antialiased`}
+        className={`${mulish.className}`}
       >
         <Nav title="Navigation" />
         {children}
--- nextjs-webapp/src/components/Nav/Nav.tsx
+++ nextjs-webapp/src/components/Nav/Nav.tsx
@@ -1,18 +1,26 @@
 "use client";
-import React from 'react';
-import Image from 'next/image';
+import React, { useState } from 'react';
 import styles from './Nav.module.scss';
+import Image from 'next/image';
 
 export type NavProps = {
 	title: string;
 }
 
 const Nav: React.FC<NavProps> = ({ }) => {
+
+	const [isVisible, setIsVisible] = useState<boolean>(false);
+
+	const toggleMenu = () => {
+		setIsVisible(!isVisible);
+	};
+
 	return (
 		<div className={styles.nav}>
-			<div className="nav-mobile">
-				<span className={`${styles.close} icon-cross`}></span>
-				<div className={styles.container}>
+			{isVisible ? (
+				<div className={styles.navMobile}>
+				<span className={`${styles.close} icon-cross`} onClick={toggleMenu}></span>
+				<div className={styles.containerMobile}>
 					<div className={styles.menu}>
 						<ul>
 							<li>Home</li>
@@ -63,19 +71,21 @@ const Nav: React.FC<NavProps> = ({ }) => {
 					</div>
 				</div>
 			</div>
-			<div className="nav-desktop">
-				<Image src="/logo.png" alt="Logo" width={100} height={50} />
-				<div className="menu-desktop">
-					<ul>
-						<li>Home</li>
-						<li>Find a doctor</li>
-						<li>Apps</li>
-						<li>Testimonials</li>
-						<li>Contact us</li>
-						<li>About us</li>
-						<li>Privacy policy</li>
-						<li>Terms of service</li>
-					</ul>
+			): null}
+			
+			<div className={`${styles.navDesktop}`}>
+				<div className={`${styles.containerDesktop}`}>
+					<Image src="logo.svg" alt="Logo" width={160} height={41} />
+					<div className={`${styles.menuDesktop}`}>
+						<ul>
+							<li>Home</li>
+							<li>Find a doctor</li>
+							<li>Apps</li>
+							<li>Testimonials</li>
+							<li>Contact us</li>
+						</ul>
+					</div>
+					<span className={`${styles.icon} icon-menu ${styles.iconMenu}`} onClick={toggleMenu}></span>
 				</div>
 			</div>
 		</div>
