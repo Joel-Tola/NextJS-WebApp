f84ffbc567c4e0bb1c5922c974a6da08bb88b3ce
Joel-Tola <SLjoel.ts@gmail.com>
2025-07-01 20:59:51 +0100

    Add theme color variable and clean up styles in Nav and MenuItem components

--- nextjs-webapp/src/app/page.tsx
+++ nextjs-webapp/src/app/page.tsx
@@ -3,7 +3,8 @@ export default function Home() {
   return (
     <div className="">
       <main className="">
-        <h1>Test</h1>
+
+        {/* <h1 className="border-b-[2px] border-blue-100">Test</h1> */}
 
       </main>
     </div>
